564df48708cde9a06c340f8171913a2f6c2ab982
virtually-ghetto <lukemhamilton@gmail.com>
2011-11-23 21:22:40 -0500

    test2

--- mvc/the_model.js
+++ mvc/the_model.js
@@ -1,6 +1,6 @@
 /* 
 
-The model is where all the application\'s data objects are stored. For example a User Model would contain a list of users, their attributes, and any logic associated specifically with that  model.
+The model is where all the application/'s data objects are stored. For example a User Model would contain a list of users, their attributes, and any logic associated specifically with that  model.
 
 A model doesn't kow about controllers / views. It should only contain its data and the logic
 directly associated with that data. ( i.e. Any event handling code, view templates, or logic
